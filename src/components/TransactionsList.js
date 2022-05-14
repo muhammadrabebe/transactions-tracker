f8d0e15177ce053e14b08fe06d365aaa96c8c0b1
Muhammad Rabebe <mohamadrabebe0@gmail.com>
2022-05-15 00:37:44 +0300

    added all api request

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 import Transaction from './Transaction';
+
 export const TransactionsList = () => {
-    const { transactions } = useContext(GlobalContext);
+    const { transactions, getTransactions } = useContext(GlobalContext);
+    useEffect(() => {
+        getTransactions();
+    }, [])
     return (
         <>
             <h3>History</h3>
             <ul id="list" className="list">
                 {transactions.map(transaction =>
                 (
-                    <Transaction transaction={transaction} key={transaction.id} />
+                    <Transaction transaction={transaction} key={transaction._id} />
                 ))}
             </ul>
         </>
